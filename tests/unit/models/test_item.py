b38e9c3df75346a05b3fbb789a7a564193b1732d
inasahu <Anurag.Sahu@vanderlande.com>
2020-02-26 14:04:51 +0530

    Complete REST API Code Testing

---
+++ tests/unit/models/test_item.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from models.item import ItemModel
+
+
+class ItemTest(TestCase):
+
+    def test_create_item(self):
+        item = ItemModel('test', 19.99)
+
+        self.assertEqual(item.name, 'test')
+        self.assertEqual(item.price, 19.99)
+
+    def test_json(self):
+        item = ItemModel('test', 19.99)
+        expected = {'name': 'test', 'price': 19.99}
+
+        self.assertDictEqual(item.json(), expected)
